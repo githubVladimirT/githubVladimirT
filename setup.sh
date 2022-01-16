fed05c45939ea18e7894db396078fd157a61f00a
githubVladimirT <vladimir.tolstov@selfip.ru>
2022-01-16 18:39:16 +0300

    Updated Github profile

--- setup.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/githubVladimirT/Cardioid.git # Clone repo
-cd Cardioid # Change dirictory to cloned
-pip3 install -r requirements.txt # Install or update modules
-chmod +x master.py
-./master.py # Run Cardioid
